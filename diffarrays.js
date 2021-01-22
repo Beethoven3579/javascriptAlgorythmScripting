46fadc8228307f8999a45dec611af9a181788aea
Beethoven3579 <75546304+Beethoven3579@users.noreply.github.com>
2021-01-21 21:11:33 -0500

    Update diffarrays.js

--- diffarrays.js
+++ diffarrays.js
@@ -41,3 +41,16 @@ function diffArray(arr1, arr2) {
   }
   
   console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
+// New solution using ES7 musch more concise!
+
+function diffArray(arr1, arr2) {
+
+let difference = arr1
+                 .filter(x => !arr2.includes(x))
+                 .concat(arr2.filter(x => !arr1.includes(x)));
+
+  return difference;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
