f0841a06d2459066a0de7471112752fb2679de32
Beethoven3579 <75546304+Beethoven3579@users.noreply.github.com>
2021-01-21 21:09:50 -0500

    Return Differences of Arrays

---
+++ diffarrays.js
@@ -0,0 +1,43 @@
+/* Intermediate Algorithm Scripting: Diff Two Arrays
+
+Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+
+Note
+You can return the array with its elements in any order.
+
+*/
+
+// For Loop Solution
+
+function diffArray(arr1, arr2) {
+    var result = [];
+  
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr2.indexOf(arr1[i]) === -1) {
+        result.push(arr1[i]);
+      }
+    }
+  
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1.indexOf(arr2[j]) === -1) {
+        result.push(arr2[j]); 
+      }
+    }
+    return result;
+  }
+  console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
+  // Filter solution
+
+  function diffArray(arr1, arr2) {
+    var combo = arr1.concat(arr2);
+  
+  
+    return combo.filter(function(num) {
+      if (arr1.indexOf(num) === -1 || arr2.indexOf(num) === -1) {
+        return num;
+      }
+    });
+  }
+  
+  console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
